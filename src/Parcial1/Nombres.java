df0655af772d0009ec0d483b542810e7ba9bd2de
Julian <julianmandujanotovar2@gmail.com>
2026-08-28 18:17:18 -0600

    Update

--- src/Parcial1/Nombres.java
+++ src/Parcial1/Nombres.java
@@ -4,7 +4,7 @@ public class Nombres {
     static void main(String[] args) {
         String nombreCompleto = "Julian Mandujano Tovar";
         String tituloPelicula = "Spider-Man Brand New Day";
-        String sistemaOperativo = "Windows";
+        String sistemaOperativo = "Windows11";
         String correoInstitucional = "25030425@itcelaya.edu.mx";
 
     }
