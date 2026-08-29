d34f56f6481caaefc72899666ebe940cff2f3207
Julian <julianmandujanotovar2@gmail.com>
2026-08-28 18:41:25 -0600

    Update

---
+++ src/Parcial1/ifElse.java
@@ -0,0 +1,10 @@
+package Parcial1;
+
+public class ifElse {
+    static void main(String[] args) {
+        int edad = 15
+            if(edad >= 18){
+                System.out.println("puedes jugar Gta, todo..");
+            }
+    }
+}
---
+++ src/Parcial1/ifSencillo.java
@@ -0,0 +1,15 @@
+package Parcial1;
+
+public class ifSencillo {
+    static void main() {
+        int corazones = 13;
+        if(corazones == 13) {
+            System.out.println("completo");
+        }
+        if(corazones > 13) {
+            System.out.println("Tienes Mods");
+        }
+        System.out.println("Fin Del Programa");
+
+    }
+}
