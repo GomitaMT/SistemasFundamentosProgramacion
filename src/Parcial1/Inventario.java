03e6618ac09e5baf72be6636ebd5f350ac5e73d2
Julian <julianmandujanotovar2@gmail.com>
2026-09-08 13:50:10 -0600

    Uppdate : Inventario

--- src/Parcial1/Inventario.java
+++ src/Parcial1/Inventario.java
@@ -4,20 +4,23 @@ import java.util.Scanner;
 
 public class Inventario {
     static void main() {
+
         Scanner sc = new Scanner(System.in);
-        int tarimas = 10;
+
         System.out.println("Nombre del producto: ");
         String producto = sc.nextLine();
-
         System.out.println("Cantidad del producto: ");
-        int cantidad = sc.nextInt();
-
-        System.out.println("Cuantos productos caben en cada tarima? ");
         int cantidadProductos = sc.nextInt();
+        System.out.println("Cuantos productos caben en cada tarima? ");
+        int capacidadTarima = sc.nextInt();
+        int tarimas_Llenas = cantidadProductos/capacidadTarima;
+        int productosobrante = cantidadProductos%capacidadTarima;
 
-        int productoSobrantes = cantidadProductos;
+        System.out.println("Producto: " + producto);
+        System.out.println("Cantidad de " + producto + ": " + cantidadProductos);
+        System.out.println("Tarimas completas: " + tarimas_Llenas);
+        System.out.println("Producto sobrante: " +  productosobrante);
 
-        int cantidadLlenas = cantidad - cantidadProductos;
-        System.out.println("Se pueden llenar" + cantidadLlenas);
+
     }
 }
