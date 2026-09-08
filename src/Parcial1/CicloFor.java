9a6d9e8a7cf5b22d576f23f298eb0cab61354655
Julian <julianmandujanotovar2@gmail.com>
2026-09-07 18:02:31 -0600

    add if

---
+++ src/Parcial1/CicloFor.java
@@ -0,0 +1,22 @@
+package Parcial1;
+
+public class CicloFor {
+    static void main() {
+        String nombre = "Expedition 33";
+        System.out.println(nombre.charAt(0));
+        System.out.println(nombre.charAt(1));
+        System.out.println(nombre.charAt(2));
+
+        for(int teQuiero = 0; teQuiero <= nombre.length(); teQuiero++) {
+            System.out.println(nombre.charAt(teQuiero));
+        }
+
+
+        /*
+        for(int iterador = -100; iterador <= 100; iterador = iterador + 5) {
+            System.out.println(iterador);
+        }
+
+        */
+    }
+}
